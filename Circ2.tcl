124e2f1a6f6424da3a2dc56e10fdb058e5f67014
Khalil Santana <khalilsantana@hotmail.com>
2023-11-07 18:27:24 -0300

    Disable tcl of shifter

--- Circ2.tcl
+++ Circ2.tcl
@@ -1,7 +1,7 @@
 vcom -2008 *.vhd
-vsim Circ2_n_shifter_tb
-add wave -radix unsigned *
-run 20 ps;
+# vsim Circ2_n_shifter_tb
+# add wave -radix unsigned *
+# run 20 ps;
 vsim Circ2_toplevel_tb
 add wave -radix unsigned *
 run 20 ps;
